2d955b90b8dbaa9cec9cdb9013ee7d3c9110341a
venoajie <59521823+venoajie@users.noreply.github.com>
2022-01-12 19:30:00 +0700

    Update combine_funding.py

--- combine_funding.py
+++ combine_funding.py
@@ -7,7 +7,21 @@ import requests
 @dataclass(unsafe_hash=True, slots=True)
 class Funding_Data ():
            
-    def fetch_funding_per_coin (self, coin: str):
+    def fetch_current_funding_rate (self):
+
+        '''
+        # Fetch current funding rate for all coin.
+                    
+                Return and rtype: next funding rate for selected coin
+
+        '''   
+        
+        end_point_all_coin= (f' https://ftx.com/api/funding_rates')
+                        
+        return requests.get(end_point_all_coin).json() ['result']
+    
+           
+    def fetch_next_funding_rate (self, coin: str):
 
         '''
         # Fetch next funding rate for each coin.
@@ -17,17 +31,8 @@ class Funding_Data ():
         '''   
         
         end_point_per_coin= (f' https://ftx.com/api/futures/{coin}/stats')
-        
-        try:
-            funding_rates_per_coin=(requests.get(end_point_per_coin).json()) ['result']
-                
-        except Exception as error:
-            import traceback
-            from loguru import logger as log
-            log.error(f"{error}")
-            log.error(traceback.format_exc())
             
-        return funding_rates_per_coin
+        return requests.get(end_point_per_coin).json() ['result']
 
     def combining_funding_rate (self):
 
@@ -38,46 +43,60 @@ class Funding_Data ():
 
         '''   
         
-        funding_rates_all = self.tarik_data_funding_all()
+        # fetch current funding rate data
+        funding_rates_all = self.fetch_current_funding_rate()
+
+        # obtain most recent funding time (there were some funding time contained in the endpoint result)
+        funding_rates_fut_max_time = max( [ (o['time']) for o in [o for o in funding_rates_all  ]])
+
+        # update coin funding rate based on the most recent funding time 
+        funding_rates_fut_ =  [ o for o in [o for o in funding_rates_all \
+            if o['time'] == funding_rates_fut_max_time  ]]
         
-        try:
- 
-            # obtain most recent funding time (there were some funding time contained in the endpoint result)
-            funding_rates_fut_max_time = max( [ (o['time']) for o in [o for o in funding_rates_all  ]])
-
-            # update coin funding rate based on the most recent funding time 
-            funding_rates_fut_ =  [ o for o in [o for o in funding_rates_all \
-                if o['time'] == funding_rates_fut_max_time  ]]
+        # prepare symbol coins
+        symbols =  [ o['future'] for o in [o for o in funding_rates_fut_  ]]           
+        
+        data_funding =[]
+        for coin in symbols:
             
-            # prepare symbol coins
-            symbols =  [ o['future'] for o in [o for o in funding_rates_fut_  ]]           
+            # obtain funding next rate based on individual coin
+            nextFundingRate_= self.fetch_next_funding_rate (coin)  
+            nextFundingRate = nextFundingRate_['nextFundingRate']  
+            nextFundingTime = (nextFundingRate_['nextFundingTime']  )
+
+            # obtain current funding time
+            currentFundingTime =  [ o['time'] for o in [o for o in funding_rates_fut_ \
+                if o['future'] == coin]][0]                
+
+            # combining current and next coins rate
+            dicttemp = {}                
+            dicttemp = {'future': coin ,
+                        'nextRate': nextFundingRate,
+                        'nextFundingTime': nextFundingTime,
+                        'time': currentFundingTime,
+                        }
             
-            data_funding =[]
-            for coin in symbols:
-                
-                # obtain funding next rate based on individual coin
-                funding_rates_next_= self.fetch_funding_per_coin(coin)  
-                funding_rates_next = funding_rates_next_['nextFundingRate']  
-                nextFundingTime = (funding_rates_next_['nextFundingTime']  )
-
-                # obtain current funding time
-                currentFundingTime =  [ o['time'] for o in [o for o in funding_rates_fut_ \
-                    if o['future'] == coin]][0]                
-
-                # combining current and next coins rate
-                dicttemp = {}                
-                dicttemp = {'future': coin ,
-                            'nextRate': funding_rates_next,
-                            'nextFundingTime': nextFundingTime,
-                            'time': currentFundingTime,
-                            }
-                
-                data_funding_ = data_funding.append(dicttemp.copy())
-                                
-        except Exception as error:
-            import traceback
-            from loguru import logger as log
-            log.error(f"{error}")
-            log.error(traceback.format_exc())
+            data_funding_ = data_funding.append(dicttemp.copy())        
                     
         return data_funding
+
+
+def main():
+    funding_data=Funding_Data()
+    return funding_data.combining_funding_rate()
+
+if __name__ == "__main__":
+
+    try:
+        main=main()
+        print(main)
+            
+    except Exception as error:
+        import traceback
+        from loguru import logger as log
+        log.error(f"{error}")
+        log.error(traceback.format_exc())
+    
+    
+    
+    
