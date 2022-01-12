70e0147794cb8f5f59ace09325e38ccec0060cc1
venoajie <59521823+venoajie@users.noreply.github.com>
2022-01-12 18:53:29 +0700

    Update combine_funding.py

--- combine_funding.py
+++ combine_funding.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from functools import lru_cache
 from loguru import logger as log
-from unsync import unsync
 from dataclassy import dataclass
 import requests
 
 @dataclass(unsafe_hash=True, slots=True)
 class Funding_Data ():
            
-    @lru_cache(maxsize=None)
     def fetch_funding_per_coin (self, coin: str):
 
         '''
@@ -20,19 +17,18 @@ class Funding_Data ():
 
         '''   
         
-        end_point_per_koin= (f' https://ftx.com/api/futures/{coin}/stats')
+        end_point_per_coin= (f' https://ftx.com/api/futures/{coin}/stats')
         
         try:
-            funding_rates_per_koin=(requests.get(end_point_per_koin).json()) ['result']
+            funding_rates_per_coin=(requests.get(end_point_per_coin).json()) ['result']
                 
         except Exception as error:
             import traceback
             log.error(f"{error}")
             log.error(traceback.format_exc())
             
-        return funding_rates_per_koin
+        return funding_rates_per_coin
 
-    @unsync
     def combining_funding_rate (self):
 
         '''
@@ -59,13 +55,16 @@ class Funding_Data ():
             data_funding =[]
             for coin in symbols:
                 
+                # obtain funding next rate based on individual coin
                 funding_rates_next_= self.fetch_funding_per_coin(coin)  
                 funding_rates_next = funding_rates_next_['nextFundingRate']  
                 nextFundingTime = (funding_rates_next_['nextFundingTime']  )
+
+                # obtain current funding time
                 currentFundingTime =  [ o['time'] for o in [o for o in funding_rates_fut_ \
                     if o['future'] == coin]][0]                
 
-                # comhining current and next coins rate
+                # combining current and next coins rate
                 dicttemp = {}                
                 dicttemp = {'future': coin ,
                             'nextRate': funding_rates_next,
